9e032b87fef98513642fb9766e25f68c0fb08fd9
Jon Galloway <jongalloway@gmail.com>
2023-10-13 15:44:31 -0700

    Added Spectre console and Magick.NET integration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ImageMagick;
+using Spectre.Console;
+using Microsoft.Extensions.Configuration;
 using Octokit;
 
 // Repos sourced from https://github.com/dotnet/core/blob/main/Documentation/core-repos.md
@@ -50,7 +52,7 @@ var config = new ConfigurationBuilder()
 string token = config["GitHubToken"];
 if (token == null)
 {
-    Console.WriteLine("GitHubToken config setting missing. Configure a token at https://github.com/settings/tokens and add it to user secrets.");
+    AnsiConsole.WriteLine("GitHubToken config setting missing. Configure a token at https://github.com/settings/tokens and add it to user secrets.");
     Environment.Exit(1);
 }
 
@@ -63,15 +65,21 @@ var github = new GitHubClient(new ProductHeaderValue("GitHubAvatarDownloader")) 
 //Using a hashset to only show an avatar once
 var allContributors = new HashSet<RepositoryContributor>();
 
-foreach (var repo in repos)
-{
-    var contributors = await github.Repository.GetAllContributors(repo.Item1, repo.Item2);
-
-    foreach (var contributor in contributors)
+await AnsiConsole.Status()
+    .StartAsync("Getting list of contributors...", async ctx =>
     {
-        allContributors.Add(contributor);
-    }
-}
+        foreach (var repo in repos)
+        {
+            AnsiConsole.MarkupLine($"Getting contributors from {repo.Item1}/{repo.Item2}");
+            var contributors = await github.Repository.GetAllContributors(repo.Item1, repo.Item2);
+
+            foreach (var contributor in contributors)
+            {
+                allContributors.Add(contributor);
+            }
+        }
+    });
+
 
 //Create directory if it doesn't exist
 string directory = config["directory"] ?? "download";
@@ -80,26 +88,71 @@ Directory.CreateDirectory(directory);
 int avatarCount = 0;
 
 var httpClient = new HttpClient();
-foreach (var c in allContributors)
-{
-    var imageBytes = await httpClient.GetByteArrayAsync(c.AvatarUrl);
-
-    //Anything < 1.5KB is a gravatar or single color graphic
-    if (imageBytes.Length > 1500)
+await AnsiConsole.Progress()
+    .StartAsync(async ctx =>
     {
-        // Only write if file doesn't exist, useful if re-running
-        if (!File.Exists($"{directory}/{c.Login}.png"))
+        var downloadTask = ctx.AddTask("Downloading avatars...", maxValue: allContributors.Count);
+        foreach (var c in allContributors)
         {
-            await File.WriteAllBytesAsync($"{directory}/{c.Login}.png", imageBytes);
+            var imageBytes = await httpClient.GetByteArrayAsync(c.AvatarUrl);
+
+            //Anything < 1.5KB is a gravatar or single color graphic
+            if (imageBytes.Length > 1500)
+            {
+                // Only write if file doesn't exist, useful if re-running
+                if (!File.Exists($"{directory}/{c.Login}.png"))
+                {
+                    await File.WriteAllBytesAsync($"{directory}/{c.Login}.png", imageBytes);
+                }
+                avatarCount++;
+            }
+            downloadTask.Increment(1);
         }
-        avatarCount++;
+    });
+
+using var images = new MagickImageCollection();
+
+// get the files from the directory in random order
+var files = Directory.GetFiles(directory).OrderBy(x => Random.Shared.Next());
+foreach (var file in files)
+{
+    images.Add(file);
+}
+
+int actualAvatars = images.Count;
+int h = (int)Math.Round(Math.Sqrt(actualAvatars / 1.7778));
+int w = (int)Math.Round(h * 1.7778);
+
+var settings = new MontageSettings
+{
+    Geometry = new MagickGeometry(200, 200),
+    TileGeometry = new MagickGeometry($"{w}x{h}"),
+    BorderWidth = 0,
+    BackgroundColor = MagickColors.Transparent
+};
+
+int surplus = images.Count - (w * h);
+if (surplus > 0)
+{
+    AnsiConsole.WriteLine($"Removing {surplus} random avatars to fit an even multiple.");
+    for (int i = 0; i < surplus; i++)
+    {
+        // remove a random image from the middle
+        int index = Random.Shared.Next(w * h - surplus);
+        images.RemoveAt(index);
     }
 }
 
-int h = (int)Math.Round(Math.Sqrt(avatarCount / 1.7778));
-int w = (int)(h * 1.7778);
+AnsiConsole.Status()
+    .Start("Generating image...", ctx =>
+    {
+        using var result = images.Montage(settings);
+        result.Write("contributors.png");
+    });
 
-Console.WriteLine("Use the following to generate a mosaic:");
-Console.WriteLine($"magick montage -geometry 200x200+0+0 -tile {w}x{h} {directory}/*.png output.png");
-Console.WriteLine("Press any key to quit.");
+AnsiConsole.WriteLine($"Total avatars downloaded: {actualAvatars}");
+AnsiConsole.WriteLine($"Image written to contributors.png");
+AnsiConsole.WriteLine("Use the following to tweak and generate a mosaic:");
+AnsiConsole.WriteLine($"magick montage -geometry 200x200+0+0 -tile {w}x{h} {directory}/*.png contributors.png");
+AnsiConsole.WriteLine("Press any key to quit.");
 Console.ReadLine();
